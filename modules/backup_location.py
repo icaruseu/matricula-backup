f969668c249ed36d834397cb009833a2ad202b8b
Daniel Jeller <djeller@nettek.at>
2023-02-28 17:15:01 +0100

    Add s3 handling

--- modules/backup_location.py
+++ modules/backup_location.py
@@ -2,6 +2,7 @@ import time
 from pathlib import Path
 from typing import List
 
+from modules.bucket import Bucket
 from modules.context import Context
 from modules.file_cache import FileCache
 
@@ -9,27 +10,30 @@ context = Context()
 
 
 class BackupLocation:
-    path: str
+    bucket: Bucket
     file_cache: FileCache
     name: str
+    path: str
 
     def __init__(self, name: str, path: str):
+        self.bucket = Bucket(name)
         self.file_cache = FileCache(name)
         self.name = name
         self.path = path
 
     def backup(self):
+        context.log.info(f"Backing up {self.name}")
         deleted: List[str] = []
         updated: List[str] = []
         added: List[str] = []
         for cached in self.file_cache.list_all():
             if not Path(cached).is_file():
                 deleted.append(cached)
-                # TODO: Delete file in s3
+                self.bucket.delete_file(cached)
         for file in Path(self.path).rglob("*"):
             if file.is_file():
                 if file.stat().st_ctime > context.history.get_last_backup(self.path):
-                    # TODO: Upload file to s3
+                    self.bucket.sync_file(file.as_posix())
                     if self.file_cache.is_cached(file.as_posix()):
                         updated.append(file.as_posix())
                     else:
@@ -37,10 +41,6 @@ class BackupLocation:
         self.file_cache.insert_all(added)
         self.file_cache.delete_all(deleted)
         context.log.info(
-            f"Backed up {self.name}; new: {len(added)}; updated: {len(updated)}; deleted: {len(deleted)}"
+            f"Result: {len(added)} [new]\t{len(updated)} [updated]\t{len(deleted)} [deleted]"
         )
         context.history.update(self.path, time.time())
-
-    def restore(self):
-        # TODO: Download all files listed in the cache
-        pass
--- modules/bucket.py
+++ modules/bucket.py
@@ -1,3 +1,76 @@
-class bucket:
-    def __init__(self):
-        pass
+import re
+from typing import cast
+
+from botocore.exceptions import ClientError
+
+from modules.context import Context
+
+context = Context()
+
+from mypy_boto3_s3.literals import BucketLocationConstraintType
+
+EXPIRATION_NONCURRENT_DAYS = 180
+PREFIX = "img-backup--"
+STORAGE_CLASS = "DEEP_ARCHIVE"
+
+
+class Bucket:
+    name: str
+
+    def __init__(self, name: str):
+        name = PREFIX + re.sub(r"[^a-zA-Z0-9]", "-", name).lower()
+        self.name = name
+        # Create bucket if it doesn't exist
+        try:
+            context.s3_client.head_bucket(Bucket=name)
+        except ClientError as error:
+            if error.response.get("Error", {}).get("Code", "-1") == "404":
+                context.log.info(f"Creating bucket {name}")
+                context.s3_client.create_bucket(
+                    ACL="private",
+                    Bucket=name,
+                    CreateBucketConfiguration={
+                        "LocationConstraint": cast(
+                            BucketLocationConstraintType, context.aws_region
+                        ),
+                    },
+                )
+            else:
+                raise error
+        # Set bucket versioning
+        context.s3_client.put_bucket_versioning(
+            Bucket=name,
+            VersioningConfiguration={
+                "Status": "Enabled",
+            },
+        )
+        # Set bucket lifecycle
+        context.s3_client.put_bucket_lifecycle_configuration(
+            Bucket=name,
+            LifecycleConfiguration={
+                "Rules": [
+                    {
+                        "ID": "Delete_noncurrent_versions",
+                        "Status": "Enabled",
+                        "NoncurrentVersionExpiration": {
+                            "NoncurrentDays": EXPIRATION_NONCURRENT_DAYS,
+                        },
+                        "Filter": {},
+                    }
+                ]
+            },
+        )
+
+    def __file_key(self, path: str) -> str:
+        return path.lstrip("/")
+
+    def sync_file(self, path: str):
+        context.s3_client.upload_file(
+            path,
+            self.name,
+            self.__file_key(path),
+            ExtraArgs={"StorageClass": STORAGE_CLASS},
+        )
+
+    def delete_file(self, path: str):
+        context.s3_client.delete_object(Bucket=self.name, Key=self.__file_key(path))
