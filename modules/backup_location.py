016fa67cc32e3435d50c4e93015d13cf32f1f0fc
Daniel Jeller <djeller@nettek.at>
2023-03-07 16:12:15 +0100

    Add checksum test before backing up

--- modules/backup_location.py
+++ modules/backup_location.py
@@ -1,7 +1,8 @@
+import hashlib
 import sys
 import time
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import pymsteams
 
@@ -25,6 +26,13 @@ def safe_to_string(path: Path) -> str:
     return str(path).encode(fse, "surrogateescape").decode("ISO-8859-1")
 
 
+def calculate_checksum(file_path):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        checksum = hashlib.md5(data).hexdigest()
+        return checksum
+
+
 class BackupLocation:
     """Describes a backup location, including the AWS S3 bucket and file cache."""
 
@@ -43,18 +51,18 @@ class BackupLocation:
     def backup(self):
         """Backs up the files in this location.
         1. It deletes files listed in the file cache but not on the file system anymore from the S3 bucket and file cacheself.
-        2. It uploads files that have been modified since the last backup to the S3 bucket and inserts them in the file cache.
+        2. It uploads files new or changed files (different checksum) to the S3 bucket and inserts them in the file cache.
         3. Any errors are logged
         4. If there were errors, a notification is sent to MS Teams
-        5. If there were no errors, the history file is updated
         """
         context.log.info(f"Backing up {self.name}")
         deleted: List[str] = []
-        updated: List[str] = []
-        added: List[str] = []
+        updated: List[Tuple[str, str]] = []
+        added: List[Tuple[str, str]] = []
         errors: List[str] = []
+        skipped: int = 0
         bucket: Optional[Bucket] = None
-        for cached in self.file_cache.list_all():
+        for cached, _ in self.file_cache.list_all():
             if not Path(cached).is_file():
                 if not context.dry_run:
                     if not bucket:
@@ -65,34 +73,37 @@ class BackupLocation:
             if file.is_file():
                 try:
                     name = str(file)
-                    if file.stat().st_ctime > context.history.get_last_backup(
-                        self.path
-                    ):
+                    checksum = calculate_checksum(file)
+                    if self.file_cache.is_new_or_changed(name, checksum):
                         if not context.dry_run:
                             if not bucket:
                                 bucket = Bucket(self.name)
                             bucket.sync_file(name)
                         if self.file_cache.is_cached(name):
-                            updated.append(name)
+                            updated.append((name, checksum))
                         else:
-                            added.append(name)
+                            added.append((name, checksum))
+                    else:
+                        skipped += 1
                 except Exception as e:
-                    message = f"Failed to backup {safe_to_string(file)}: {e}"
+                    message = f"Failed to backup *{safe_to_string(file)}*: `{e}`"
                     context.log.error(message)
                     errors.append(message)
 
-        self.file_cache.insert_all(added)
+        self.file_cache.upsert_all(added)
+        self.file_cache.upsert_all(updated)
         self.file_cache.delete_all(deleted)
+        log_entry = f"\t{len(added)} [new]\t{len(updated)} [updated]\t{len(deleted)} [deleted]\t{len(errors)} [errors]\t{skipped} [skipped]"
         if errors:
-            context.log.error(f"Result: {len(errors)} errors")
+            context.log.error(f"Failure: {log_entry}")
             if context.notification_webhook:
                 text = "\n".join(errors)
                 message = pymsteams.connectorcard(context.notification_webhook)
-                message.title("Backup finished with errors")
+                message.title(
+                    f"Backup of '{self.name}' finished with {len(errors)} { 'error' if len(errors) == 1 else 'errors' }"
+                )
                 message.text(text)
                 message.send()
         else:
-            context.log.info(
-                f"Result: {len(added)} [new]\t{len(updated)} [updated]\t{len(deleted)} [deleted]\t{len(errors)} [errors]"
-            )
+            context.log.info(f"Success: {log_entry}")
             context.history.update(self.path, time.time())
--- modules/file_cache.py
+++ modules/file_cache.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 import urllib.parse
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 from modules.context import Context
 
@@ -18,7 +18,9 @@ class FileCache:
         os.makedirs(parent, exist_ok=True)
         self.db_path = parent.joinpath(file_name)
         con = sqlite3.connect(self.db_path)
-        con.cursor().execute("CREATE TABLE IF NOT EXISTS files (key TEXT PRIMARY KEY);")
+        con.cursor().execute(
+            "CREATE TABLE IF NOT EXISTS files (key TEXT PRIMARY KEY, checksum TEXT);"
+        )
         con.commit()
         con.close()
 
@@ -28,36 +30,52 @@ class FileCache:
         con.commit()
         con.close()
 
-    def insert_all(self, file_paths: List[str]):
+    def upsert_all(self, files: List[Tuple[str, str]]):
         con = sqlite3.connect(self.db_path)
         cursor = con.cursor()
-        for file_path in file_paths:
+        for file_path, checksum in files:
             cursor.execute(
-                f"INSERT OR IGNORE INTO files (key) VALUES (?);", (file_path,)
+                f"INSERT INTO files (key, checksum) VALUES (:key, :checksum) ON CONFLICT(key) DO UPDATE SET checksum=:checksum;",
+                {"key": file_path, "checksum": checksum},
             )
         con.commit()
         con.close()
 
-    def list_all(self) -> List[str]:
+    def list_all(self) -> List[Tuple[str, str]]:
         con = sqlite3.connect(self.db_path)
+        con.row_factory = sqlite3.Row
         cursor = con.cursor()
-        cursor.execute("SELECT key FROM files;")
+        cursor.execute("SELECT key, checksum FROM files;")
         result = cursor.fetchall()
         con.close()
-        return [row[0] for row in result]
+        return [(row["key"], row["checksum"]) for row in result]
 
     def delete_all(self, file_paths: List[str]):
         con = sqlite3.connect(self.db_path)
         cursor = con.cursor()
         for file_path in file_paths:
-            cursor.execute("DELETE FROM files WHERE key = ?;", (file_path,))
+            cursor.execute("DELETE FROM files WHERE key = :key;", {"key": file_path})
         con.commit()
         con.close()
 
     def is_cached(self, file_path: str) -> bool:
         con = sqlite3.connect(self.db_path)
         cursor = con.cursor()
-        cursor.execute("SELECT key FROM files WHERE key = ?;", (file_path,))
+        cursor.execute(
+            "SELECT key FROM files WHERE key = :key;",
+            {"key": file_path},
+        )
         result = cursor.fetchall()
         con.close()
         return len(result) > 0
+
+    def is_new_or_changed(self, file_path: str, checksum: str) -> bool:
+        con = sqlite3.connect(self.db_path)
+        cursor = con.cursor()
+        cursor.execute(
+            "SELECT key FROM files WHERE key = :key AND checksum = :checksum;",
+            {"key": file_path, "checksum": checksum},
+        )
+        result = cursor.fetchall()
+        con.close()
+        return len(result) == 0
